32dafbdd127a854b145fb6da5f5780d604aa04ac
Łukasz Uznański <lukaszuznanski94@gmail.com>
2020-10-27 16:52:56 +0100

    update version (#14)

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,12 +3,12 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Headless News',
     'description' => 'This extension provides integration with news to output content from TYPO3 in JSON format.',
-    'state' => 'beta',
+    'state' => 'stable',
     'author' => 'Łukasz Uznański',
     'author_email' => 'extensions@macopedia.com',
     'category' => 'fe',
     'internal' => '',
-    'version' => '1.0.0',
+    'version' => '1.0.1',
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-10.0.99',
