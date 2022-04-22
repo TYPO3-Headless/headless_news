5cae3675276f281e16176d9742d8731904bbc9fb
Niels Seelhöfer <derseeli@users.noreply.github.com>
2022-04-22 16:46:49 +0200

    Added possibility to format dates over typoscript + no absolute URLs in pagination (#21)
    update versions in emconf
    Added possibility to format dates over typoscript
    added more + backlink in JSON-Response for list- and detail-view
    
    Co-authored-by: Niels Seelhöfer <niels.seelhoefer@trixie.de>

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,5 +1,6 @@
 <?php
 
+/** @phpstan-ignore-next-line */
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Headless News',
     'description' => 'This extension provides integration with news to output content from TYPO3 in JSON format.',
@@ -11,12 +12,12 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.0.99',
-            'frontend' => '9.5.0-10.0.99'
+            'typo3' => '10.0.0-11.5.99',
+            'frontend' => '10.0.0-11.5.99',
         ],
         'conflicts' => [],
         'suggests' => [
-            'headless' => '2.0.0-2.9.9'
-        ]
+            'headless' => '2.0.0-3.9.9',
+        ],
     ],
 ];
