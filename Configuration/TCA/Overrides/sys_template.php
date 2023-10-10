b66277e18bc103942064b721193a9b14e3a34f97
NITSAN Technologies <sanjay@nitsan.in>
2023-10-10 13:56:39 +0530

    [TASK] Compatible with Typo3 V12 (#28)

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,13 +1,22 @@
 <?php
-defined('TYPO3_MODE') || die();
+defined('TYPO3') || die();
 
 call_user_func(function () {
     /**
      * Default TypoScript for Headless News
      */
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-        'headless_news',
-        'Configuration/TypoScript',
-        'Headless News'
-    );
+    $versionInformation = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Information\Typo3Version::class);
+    if ($versionInformation->getMajorVersion() > 11) {
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+            'headless_news',
+            'Configuration/TypoScript/v12',
+            'Headless News For V12'
+        );
+    } else {
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+            'headless_news',
+            'Configuration/TypoScript',
+            'Headless News'
+        );
+    }
 });
